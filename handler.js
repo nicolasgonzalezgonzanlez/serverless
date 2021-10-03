1cc9bf426c23bbad6d988cf7456fd7ac7b7f0571
Nicolas Gonzalez <>
2021-10-03 13:16:22 -0300

    init

--- handler.js
+++ handler.js
@@ -1,37 +1,11 @@
 'use strict';
-const queryString = require('querystring')
-module.exports.hello = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `Hola ${event.pathParameters.name}`,
-        input: event.name,
-      },
-      null,
-      2
-    ),
-  };
+const serverless = require('serverless-http')
+const express = require('express')
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-module.exports.showUser = async (event) => {
-  const body = queryString.parse(event['body'])
-  console.log(body)
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: `peticion post`,
-        input: `Hola ${body.name}`,
-      },
-      null,
-      2
-    ),
-  };
+const app = express()
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+app.get('/', (req, res) => {
+  res.send('hola mundo con express y serverless')
+})
 
+module.exports.app = serverless(app)
